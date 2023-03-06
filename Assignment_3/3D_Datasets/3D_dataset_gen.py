bb52d4c8fe65e7188c82d2752548b4d3daf0d0e9
mmg63 <ertebatat.royan@live.com>
2023-03-06 18:27:23 -0500

    .

---
+++ Assignment_3/3D_Datasets/3D_dataset_gen.py
@@ -0,0 +1,52 @@
+#%%
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+
+# unused but required import for doing 3d projections with matplotlib < 3.2
+import mpl_toolkits.mplot3d  # noqa: F401
+
+from sklearn import manifold, datasets
+
+n_samples = 1500
+S_points, S_color = datasets.make_s_curve(n_samples, random_state=0)
+swiss_points, swiss_colors = datasets.make_swiss_roll(n_samples=n_samples, random_state=0)
+#%%
+def plot_3d(points, points_color, title):
+    x, y, z = points.T
+
+    fig, ax = plt.subplots(
+        figsize=(6, 6),
+        facecolor="white",
+        tight_layout=True,
+        subplot_kw={"projection": "3d"},
+    )
+    fig.suptitle(title, size=16)
+    col = ax.scatter(x, y, z, c=points_color, s=50, alpha=0.8)
+    ax.view_init(azim=-60, elev=9)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.zaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    fig.colorbar(col, ax=ax, orientation="horizontal", shrink=0.6, aspect=60, pad=0.01)
+    plt.show()
+
+
+def plot_2d(points, points_color, title):
+    fig, ax = plt.subplots(figsize=(3, 3), facecolor="white", constrained_layout=True)
+    fig.suptitle(title, size=16)
+    add_2d_scatter(ax, points, points_color)
+    plt.show()
+
+
+def add_2d_scatter(ax, points, points_color, title=None):
+    x, y = points.T
+    ax.scatter(x, y, c=points_color, s=50, alpha=0.8)
+    ax.set_title(title)
+    ax.xaxis.set_major_formatter(ticker.NullFormatter())
+    ax.yaxis.set_major_formatter(ticker.NullFormatter())
+
+
+plot_3d(S_points, S_color, "Original S-curve samples")
+plot_3d(swiss_points, swiss_colors, "Original S-curve samples")
+
+#%%
